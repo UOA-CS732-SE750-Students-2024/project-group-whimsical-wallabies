b9038ab3b836189d5c86f9fe5eec1ff98e25f5ac
Fabio García <fgar174@aucklanduni.ac.nz>
2024-03-17 00:28:30 +1300

    Add import order plugin

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
-import express from 'express';
-const app = express();
 import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
