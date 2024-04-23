53424d77d7245691f8fde34163df3d2e6a8c2fe5
xxbeng <tanhongwei33130@gmail.com>
2024-04-23 22:22:55 +1200

    to not include the entire react module

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -10,7 +10,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { APPLICATION_PATH } from '../../utils/urlRoutes';
