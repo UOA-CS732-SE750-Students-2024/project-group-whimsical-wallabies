c2aa9e651d637a94ac484569ad808cdc220e79cd
Jinky Xiang <jxia436@aucklanduni.ac.nz>
2024-05-03 17:53:11 +1200

    remove dogservice.js

--- frontend/src/services/dogService.js
+++
@@ -1,6 +0,0 @@
-import axiosApiInstance from '../utils/axiosApiInstance';
-import { API_PATH } from '../utils/urlRoutes';
-
-export const addDog = async (data) => axiosApiInstance.post(API_PATH.dog.add, data);
-export const dogProfile = async (data) => axiosApiInstance.get(API_PATH.dog.profile, data); //not sure if this is correct
-export const dogDashboard = async (data) => axiosApiInstance.get(API_PATH.dog.dogDashboard, data); //not sure if this is correct
