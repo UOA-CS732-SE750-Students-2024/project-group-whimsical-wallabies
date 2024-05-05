b9b83397c3fa71b3afc342c9c8fe59ac0bbe0eb6
steveia3000 <69488332+steveia3000@users.noreply.github.com>
2024-05-06 04:06:59 +1200

    modify route

--- frontend/src/utils/urlRoutes.js
+++ frontend/src/utils/urlRoutes.js
@@ -23,6 +23,6 @@ export const APPLICATION_PATH = {
   matching: '/matching',
   dashboard: '/dashboard',
   homepage: '/',
-  friendlist: '/friendlist',
+  friendlist: '/friendlist:id',
   filter: '/filter'
 };
